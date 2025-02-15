5ce16e8358de50ea5a19b4852c6dd83b590ebb0d
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-02-14 20:58:40 -0800

    first draft

---
+++ R/attach.R
@@ -0,0 +1,93 @@
+core <- c("crane", "gtsummary")
+
+core_unloaded <- function() {
+  search <- paste0("package:", core)
+  core[!search %in% search()]
+}
+
+# Attach the package from the same package library it was
+# loaded from before. https://github.com/tidyverse/tidyverse/issues/171
+same_library <- function(pkg) {
+  loc <- if (pkg %in% loadedNamespaces()) dirname(getNamespaceInfo(pkg, "path"))
+  library(pkg, lib.loc = loc, character.only = TRUE, warn.conflicts = FALSE)
+}
+
+crane_attach <- function() {
+  to_load <- core_unloaded()
+  if (!is_empty(to_load)) to_load <- union("crane", y = to_load)
+
+  suppressPackageStartupMessages(
+    lapply(to_load, same_library)
+  )
+
+  invisible(to_load)
+}
+
+crane_attach_message <- function(to_load) {
+  if (length(to_load) == 0) {
+    return(NULL)
+  }
+
+  header <- cli::rule(
+    left = cli::style_bold("Attaching packages")
+  )
+
+  to_load <- sort(to_load)
+  versions <- vapply(to_load, package_version_h, character(1))
+
+  packages <- paste0(
+    cli::col_green(cli::symbol$tick), " ", cli::col_blue(format(to_load)), " ",
+    cli::ansi_align(versions, max(cli::ansi_nchar(versions)))
+  )
+
+  if (length(packages) %% 2 == 1) {
+    packages <- append(packages, "")
+  }
+  col1 <- seq_len(length(packages) / 2)
+  info <- paste0(packages[col1], "     ", packages[-col1])
+
+  paste0(header, "\n", paste(info, collapse = "\n"))
+}
+
+package_version_h <- function(pkg) {
+  highlight_version(utils::packageVersion(pkg))
+}
+
+highlight_version <- function(x) {
+  x <- as.character(x)
+
+  is_dev <- function(x) {
+    x <- suppressWarnings(as.numeric(x))
+    !is.na(x) & x >= 9000
+  }
+
+  pieces <- strsplit(x, ".", fixed = TRUE)
+  pieces <- lapply(pieces, function(x) ifelse(is_dev(x), cli::col_red(x), x))
+  vapply(pieces, paste, collapse = ".", FUN.VALUE = character(1))
+}
+
+
+# copied largely from the tidyverse repo, utils.R file 2025-02-14
+inform_startup <- function(msg, ...) {
+  if (is.null(msg)) {
+    return()
+  }
+  if (isTRUE(getOption("crane.quiet"))) {
+    return()
+  }
+
+  rlang::inform(msg, ..., class = "packageStartupMessage")
+}
+
+invert <- function(x) {
+  if (length(x) == 0) return()
+  stacked <- utils::stack(x)
+  tapply(as.character(stacked$ind), stacked$values, list)
+}
+
+# run the Roche theme function
+crane_theme_message <- function() {
+  header <- cli::rule(left = paste(cli::style_bold("Initializing Roche ", cli::col_blue("gtsummary"), " theme")))
+  theme_info <- cli::format_inline("See {.help crane::theme_gtsummary_roche} for details.")
+  paste0(header, "\n", theme_info)
+}
