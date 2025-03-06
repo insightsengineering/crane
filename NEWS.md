e8f2fcaba1f7d1d76c525a0d8664c3304f593e3d
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-03-06 15:47:10 -0800

    Adding `add_overall()` (#32)
    **What changes are proposed in this pull request?**
    * Update the `tbl_demographics()` returned `inputs` list to match the
    `tbl_demographics()` argument inputs, whereas it previously returned the
    `tbl_summary()` inputs. (#31)
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #<issue number>'_
    closes #31
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [ ] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,3 @@
 # crane 0.1.0.9000
 
-* Initial CRAN submission.
+* Update the `tbl_demographics()` returned `inputs` list to match the `tbl_demographics()` argument inputs, whereas it previously returned the `tbl_summary()` inputs. (#31)
--- tests/testthat/test-tbl_demographics.R
+++ tests/testthat/test-tbl_demographics.R
@@ -1,3 +1,5 @@
+skip_if_not(is_pkg_installed("withr"))
+
 test_that("tbl_demographics() works", {
   expect_silent(
     tbl <-
@@ -6,5 +8,42 @@ test_that("tbl_demographics() works", {
         by = trt,
         include = c(age, grade)
       )
+  )
+})
+
+test_that("tbl_demographics() |> add_overall() works", {
+  withr::local_options(list(width = 300))
+  expect_equal(
+    tbl_demographics(
+      trial,
+      by = trt,
+      include = c(age, grade),
+      digits = list(grade = list(p = 1)),
+      nonmissing = "no"
+    ) |>
+      add_overall() |>
+      as.data.frame(),
+    tbl_summary(
+      trial,
+      by = trt,
+      include = c(age, grade),
+      type = list(age = "continuous2"),
+      statistic = list(age = c("{mean} ({sd})", "{median}", "{min} - {max}")),
+      digits = list(grade = list(p = 1)),
+      missing = "no"
+    ) |>
+      add_overall() |>
+      as.data.frame()
+  )
+
+  expect_snapshot(
+    tbl_demographics(
+      trial,
+      by = trt,
+      digits = list(grade = list(p = 1)),
+      include = c(age, grade)
+    ) |>
+      add_overall() |>
+      as.data.frame()
   )
 })
