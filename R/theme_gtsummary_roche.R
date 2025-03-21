7eb44caadcde1c94efb020dced18a304fc59151e
Melkiades <davide.garolini@roche.com>
2025-03-21 17:49:57 +0100

    initial considerations

--- R/theme_gtsummary_roche.R
+++ R/theme_gtsummary_roche.R
@@ -17,7 +17,8 @@
 #' tbl_demographics(trial, by = trt, include = c(age, grade))
 #'
 #' reset_gtsummary_theme()
-theme_gtsummary_roche <- function(set_theme = TRUE, font_size = NULL) {
+theme_gtsummary_roche <- function(set_theme = TRUE, font_size = NULL,
+                                  border = flextable::fp_border_default(width = 1, color = "black")) {
   # start with the compact theme -----------------------------------------------
   lst_theme <- gtsummary::theme_gtsummary_compact(set_theme = FALSE, font_size = font_size)
   lst_theme$`pkgwide-str:theme_name` <- "Roche"
@@ -32,19 +33,52 @@ theme_gtsummary_roche <- function(set_theme = TRUE, font_size = NULL) {
       )
     )
 
+
   # use arial font for flextables ----------------------------------------------
   lst_theme$`as_flex_table-lst:addl_cmds` <-
     c(
       lst_theme$`as_flex_table-lst:addl_cmds`,
-      list(valign = rlang::expr(flextable::font(fontname = "Arial", part = "all")))
+      list(
+        user_added1 = rlang::expr(flextable::font(fontname = "Arial", part = "all")),
+        user_added1 = rlang::expr(flextable::fontsize(size = 7, part = "footer"))#,
+        # user_added1 = rlang::expr(
+        #   flextable::border_outer(part = "body", border = border) %>%
+        #     flextable::border_outer(part = "header", border = border) %>%
+        #     flextable::border(
+        #       part = "header", j = 1,
+        #       border.left = border,
+        #       border.right = border
+        #     ) %>%
+        #     flextable::border(
+        #       part = "header", j = 1, i = 1,
+        #       border.top = border
+        #     ) # %>%
+            # flextable::border(
+            #   part = "header", j = 1, i = flextable::nrow_part(flx, "header"),
+            #   border.bottom = border
+            # ) %>%
+            # flextable::border(
+            #   part = "header", j = seq(2, ncol),
+            #   border.left = border,
+            #   border.right = border
+            # )
+        # )
+      )
     )
+  # todo:
+  # * Bold label rows
+  # * single line spacing
+  # * vertical padding
+  # * margins
+  # (*) Page sizes defaults -> into docx save along with pagination (? or not)
+  # (*) flextable::fix_border_issues(flx) # Fixes some rendering gaps in borders
 
 
   # add a monospace font for gt ------------------------------------------------
   lst_theme$`as_gt-lst:addl_cmds` <-
     c(
       lst_theme$`as_gt-lst:addl_cmds`,
-      list(tab_spanner = rlang::expr(gt::opt_table_font(stack = "monospace-code")))
+      list(font_type = rlang::expr(gt::opt_table_font(stack = "monospace-code")))
     )
 
 
