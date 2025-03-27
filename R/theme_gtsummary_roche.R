80d06f4a9ca097eb815d53952a10f6740868039e
Melkiades <davide.garolini@roche.com>
2025-03-27 11:11:54 +0100

    feature: bold and borders

--- R/theme_gtsummary_roche.R
+++ R/theme_gtsummary_roche.R
@@ -7,6 +7,8 @@
 #' - Defaults to a mono-spaced font for gt tables.
 #'
 #' @inheritParams gtsummary::theme_gtsummary_compact
+#' @param border (`fp_border`)\cr
+#'   Border style. Defaults to `flextable::fp_border_default(width = 0.5)`.
 #'
 #' @return theme list
 #' @export
@@ -17,8 +19,7 @@
 #' tbl_demographics(trial, by = trt, include = c(age, grade))
 #'
 #' reset_gtsummary_theme()
-theme_gtsummary_roche <- function(set_theme = TRUE, font_size = NULL,
-                                  border = flextable::fp_border_default(width = 1, color = "black")) {
+theme_gtsummary_roche <- function(set_theme = TRUE, font_size = 8, border = flextable::fp_border_default(width = 0.5)) {
   # start with the compact theme -----------------------------------------------
   lst_theme <- gtsummary::theme_gtsummary_compact(set_theme = FALSE, font_size = font_size)
   lst_theme$`pkgwide-str:theme_name` <- "Roche"
@@ -29,6 +30,7 @@ theme_gtsummary_roche <- function(set_theme = TRUE, font_size = NULL,
       list(
         "tbl_summary-fn:percent_fun" = gtsummary::label_style_number(digits = 1, scale = 100),
         "pkgwide-fn:pvalue_fun" = label_roche_pvalue(),
+        "pkgwide-fun:pre_conversion" = gtsummary::bold_labels(),
         "pkgwide-str:print_engine" = "flextable"
       )
     )
@@ -39,39 +41,36 @@ theme_gtsummary_roche <- function(set_theme = TRUE, font_size = NULL,
     c(
       lst_theme$`as_flex_table-lst:addl_cmds`,
       list(
-        user_added1 = rlang::expr(flextable::font(fontname = "Arial", part = "all")),
-        user_added1 = rlang::expr(flextable::fontsize(size = 7, part = "footer"))#,
-        # user_added1 = rlang::expr(
-        #   flextable::border_outer(part = "body", border = border) %>%
-        #     flextable::border_outer(part = "header", border = border) %>%
-        #     flextable::border(
-        #       part = "header", j = 1,
-        #       border.left = border,
-        #       border.right = border
-        #     ) %>%
-        #     flextable::border(
-        #       part = "header", j = 1, i = 1,
-        #       border.top = border
-        #     ) # %>%
-            # flextable::border(
-            #   part = "header", j = 1, i = flextable::nrow_part(flx, "header"),
-            #   border.bottom = border
-            # ) %>%
-            # flextable::border(
-            #   part = "header", j = seq(2, ncol),
-            #   border.left = border,
-            #   border.right = border
-            # )
-        # )
+        fontsize = rlang::expr(flextable::fontsize(size = !!font_size - 1, part = "footer")),
+        border = rlang::expr(
+          flextable::border_outer(part = "body", border = !!border)
+        ),
+        border = rlang::expr(
+          flextable::border_outer(part = "header", border = !!border)
+        ),
+        valign = rlang::expr(flextable::valign(valign = "top", part = "all")),
+        valign = rlang::expr(flextable::font(fontname = "Arial", part = "all")), # valign only bc last cmd
+        valign = rlang::expr(flextable::line_spacing(space = 1, part = "all"))
       )
     )
   # todo:
-  # * Bold label rows
-  # * single line spacing
-  # * vertical padding
-  # * margins
-  # (*) Page sizes defaults -> into docx save along with pagination (? or not)
-  # (*) flextable::fix_border_issues(flx) # Fixes some rendering gaps in borders
+  # * vertical padding different for label rows (3 + 0.2)
+  # * horizontal padding has various different defaults
+  # * section_properties (landscape e.g.): officer::body_set_default_section(doc, section_properties)
+  # * metadata: do.call(officer::set_doc_properties, c(list("x" = doc), doc_metadata))
+#   } else { # A4
+# page_size <- officer::page_size(
+#     orient = orientation,
+#     width = 8.27, height = 11.69
+#   )
+# }
+#
+# # Final output
+# officer::prop_section(
+#   page_size = page_size,
+#   type = "continuous",
+#   page_margins = margins_potrait()
+# )
 
 
   # add a monospace font for gt ------------------------------------------------
