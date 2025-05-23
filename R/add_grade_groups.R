952cf6a53664a0425d3245aca00a6dc99cc0fadf
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-05-22 20:38:02 -0400

    Add ordering

--- R/add_grade_groups.R
+++ R/add_grade_groups.R
@@ -9,8 +9,8 @@
 #' )
 #'
 #' tbl_hierarchical_groups(
-#'   adae,
-#'   variables = AETOXGR,
+#'   adae, #|> mutate(AETOXGR = factor(AETOXGR, ordered = TRUE)),
+#'   variables = c(AEBODSYS, AEDECOD, AETOXGR),
 #'   level_groups = grade_groups,
 #'   id = USUBJID,
 #'   denominator = adsl,
@@ -42,6 +42,8 @@ tbl_hierarchical_groups <- function(data,
     # drop levels from levels_exclude
     filter(!var %in% levels_exclude)
 
+  # browser()
+
   ard <-
     ard_stack_hierarchical(
       data = data_no_gps,
@@ -57,17 +59,19 @@ tbl_hierarchical_groups <- function(data,
   if (length(level_groups) > 0) {
     data_gps <- data
 
-    ## groups must be mutually exclusive
+    ## groups must be mutually exclusive?
 
-    gps <- sapply(seq_along(level_groups), \(x) level_groups[[x]][[3]])
+    gp_nms <- sapply(seq_along(level_groups), \(x) level_groups[[x]][[3]])
+    gps <- lapply(seq_along(level_groups), \(x) (level_groups[[x]][[2]] |> eval())) |> setNames(gp_nms)
     ord <- is.ordered(data_gps[[var]])
+    by_cols <- if (!is.null(by)) c("group1", "group1_level") else NULL
 
     data_gps[[var]] <-
       do.call(
         dplyr::case_match,
         args = c(list(.x = data_gps[[var]]), level_groups)
       ) |>
-      factor(levels = gps, ordered = ord)
+      factor(levels = gp_nms, ordered = ord)
 
     ard_gps <-
       ard_stack_hierarchical(
@@ -87,13 +91,32 @@ tbl_hierarchical_groups <- function(data,
     ) |>
       sort_ard_hierarchical("alphanumeric")
 
-    browser()
-    # arrange
+    lvls <- ard$variable_level[!unlist(ard$variable_level) %in% gp_nms] |> unlist() |> as.character() |> unique()
 
-    ard |>
-      group_by(pick(all_ard_groups(), "variable")) |>
-      group_keys()
-
+    # arrange with groups prior to their first level
+    ard <- ard |>
+      mutate(idx = dplyr::row_number()) |>
+      group_by(pick(all_ard_groups(), "variable", -by_cols)) |>
+      group_split() |>
+      map(function(dat) {
+        if (dat$variable[1] != var) {
+          dat
+        } else {
+          dat$idx_lvl <- NA
+          dat$idx_lvl[!unlist(dat$variable_level) %in% gp_nms] <-
+            sapply(dat$variable_level[!unlist(dat$variable_level) %in% gp_nms], \(x) which(lvls == unlist(x)))
+          dat$idx_lvl[unlist(dat$variable_level) %in% gp_nms] <-
+            sapply(dat$variable_level[unlist(dat$variable_level) %in% gp_nms], \(x) min(which(lvls %in% gps[[unlist(x)]])) - 0.5)
+          dat |>
+            arrange(idx_lvl, idx) |>
+            mutate(idx = seq(min(idx), max(idx))) |>
+            select(-idx_lvl)
+        }
+      }) |>
+      bind_rows() |>
+      arrange(idx) |>
+      select(-idx) |>
+      as_card()
   }
 
   tbl <- tbl_ard_hierarchical(
