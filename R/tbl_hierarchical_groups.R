847a6936648736a83e03c4b41dd4d21182ea3fed
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-05-26 23:19:37 -0400

    Use tables instead of ARDs

--- R/tbl_hierarchical_groups.R
+++ R/tbl_hierarchical_groups.R
@@ -31,12 +31,13 @@ tbl_hierarchical_groups <- function(data,
                                     id,
                                     denominator,
                                     by = NULL,
-                                    include = last(variables),
+                                    include = dplyr::last(variables),
                                     include_overall = everything(),
                                     statistic = everything() ~ "{n} ({p}%)",
                                     labels = NULL,
                                     digits = NULL,
-                                    sort = "descending",
+                                    sort = "alphanumeric",
+                                    filter = NULL,
                                     level_groups = list(),
                                     levels_exclude = NULL) {
   # check inputs ---------------------------------------------------------------
@@ -44,10 +45,11 @@ tbl_hierarchical_groups <- function(data,
   cards::process_selectors(data, variables = {{ variables }}, by = {{ by }}, id = {{ id }})
   cards::process_selectors(data[variables], include = {{ include }}, include_overall = {{ include_overall }})
 
-  var <- last(variables)
+  filter <- enquo(filter)
+  var <- dplyr::last(variables)
   ord <- is.ordered(data[[var]])
-  by_cols <- if (!is.null(by)) c("group1", "group1_level") else NULL
   lvls <- levels(data[[var]])
+  include_all <- union(include, variables)
 
   if (is.null(labels)) {
     labels <- lapply(variables, \(x) attr(data[[x]], "label") %||% x) |> setNames(variables)
@@ -65,151 +67,152 @@ tbl_hierarchical_groups <- function(data,
   for (v in setdiff(include_overall, variables[c(1, length(variables))])) {
     data_list[[1]] <- data_list[[1]] |>
       mutate(!!v := "- Overall -") |>
-      bind_rows(data_list[[1]])
+      dplyr::bind_rows(data_list[[1]])
   }
 
   # ungrouped data -----
-  ard_list <- lapply(
-    seq_along(data_list),
+  tbl_list <- lapply(
+    rev(seq_along(data_list)),
     function(i) {
-      data_no_gps <- data_list[[i]] |>
-        # drop levels from levels_exclude
-        filter(!.data[[var]] %in% levels_exclude)
-
-      tbl <-
-        tbl_hierarchical(
-          data_no_gps,
-          variables,
-          id,
-          denominator,
-          by,
-          {{ include }},
-          {{ statistic }},
-          overall_row = FALSE,
-          labels,
-          {{ digits }}
-        )
-
-      ard <- gather_ard(tbl)[[1]]
-
-      if (i > 1) {
-        ard |>
-          filter(variable %in% c(variables[1], var)) |>
-          select(-all_ard_groups("levels"), by_cols, group2_level)
-      } else {
-        ard
-      }
+      tbl_hierarchical(
+        data_list[[i]],
+        variables,
+        id,
+        denominator,
+        by,
+        include_all,
+        {{ statistic }},
+        overall_row = FALSE,
+        labels,
+        {{ digits }}
+      )
     }
   )
 
   # grouped data -------
   if (length(level_groups) > 0) {
-
     ## groups mutually exclusive?
     ## at least 2 vars
 
-    ovrl_gr <- var %in% include_overall
     gp_nms <- sapply(seq_along(level_groups), \(x) level_groups[[x]][[3]])
     gps <- lapply(seq_along(level_groups), \(x) (level_groups[[x]][[2]] |> eval())) |> setNames(gp_nms)
 
-    ard_list <- c(
-      ard_list,
+    tbl_list <- c(
+      tbl_list,
       lapply(
-        seq_along(if (ovrl_gr) rep(data_list, 2) else data_list),
+        rev(seq_along(data_list)),
         function(i) {
-          which_data <- if (ovrl_gr) rep(seq_along(data_list), 2)[i] else i
-          data <- data_list[[which_data]]
-
-          data[[var]] <- if (i > length(data_list)) {
-            rep("- Any Grade -", nrow(data)) |>
-              factor(levels = "- Any Grade -", ordered = ord)
-          } else {
+          data <- data_list[[i]]
+          data[[var]] <-
             do.call(
               dplyr::case_match,
               args = c(list(.x = data[[var]]), level_groups)
             ) |>
             factor(levels = gp_nms, ordered = ord)
-          }
 
-          tbl <-
-            tbl_hierarchical(
-              data,
-              variables,
-              id,
-              denominator,
-              by,
-              {{ include }},
-              {{ statistic }},
-              overall_row = FALSE,
-              labels,
-              {{ digits }}
-            ) |>
+          tbl_hierarchical(
+            data,
+            variables,
+            id,
+            denominator,
+            by,
+            include_all,
+            {{ statistic }},
+            overall_row = FALSE,
+            labels,
+            {{ digits }}
+          ) |>
             suppressMessages()
-
-          ard <- gather_ard(tbl)[[1]]
-
-          if (which_data > 1) {
-            ard |>
-              filter(variable %in% c(variables[1], var)) |>
-              select(-all_ard_groups("levels"), by_cols, group2_level)
-          } else {
-            ard
-          }
         }
       )
     )
 
-    ard <- bind_ard(
-      ard_list,
-      .quiet = TRUE
-    ) |>
-      sort_ard_hierarchical(sort)
+    tbl <- tbl_stack(tbl_list)
+    class(tbl) <- class(tbl_list[[1]])
+    tbl$cards <- list(tbl_hierarchical = dplyr::bind_rows(sapply(tbl_list, \(x) x$cards)))
+    tbl <- tbl |> sort_hierarchical(sort)
+    tbl$cards$tbl_hierarchical <- tbl$cards$tbl_hierarchical |>
+      dplyr::distinct(dplyr::pick(-.data$fmt_fn), .keep_all = TRUE)
+    if (!quo_is_null(filter)) tbl <- tbl |> filter_hierarchical({{ filter }})
 
     # arrange with groups prior to their first level
-    ard <- ard |>
-      mutate(idx = dplyr::row_number()) |>
-      group_by(pick(all_ard_groups(), "variable", -by_cols)) |>
-      group_split() |>
-      map(function(dat) {
-        if (dat$variable[1] != var) {
-          dat
-        } else {
-          dat$idx_lvl <- NA
-          dat$idx_lvl[unlist(dat$variable_level) == "- Any Grade -"] <- 0
-          if (any(unlist(dat$variable_level) %in% lvls)) {
-            dat$idx_lvl[is.na(dat$idx_lvl) & !unlist(dat$variable_level) %in% gp_nms] <-
-              sapply(
-                dat$variable_level[is.na(dat$idx_lvl) & !unlist(dat$variable_level) %in% gp_nms],
-                \(x) which(lvls == unlist(x))
-              )
-          }
-          if (any(unlist(dat$variable_level) %in% gp_nms)) {
-            dat$idx_lvl[unlist(dat$variable_level) %in% gp_nms] <-
-              sapply(
-                dat$variable_level[unlist(dat$variable_level) %in% gp_nms],
-                \(x) min(which(lvls %in% gps[[unlist(x)]])) - 0.5
-              )
-          }
-          dat |>
-            arrange(idx_lvl, idx) |>
-            mutate(idx = seq(min(idx), max(idx))) |>
-            select(-idx_lvl)
+    tbl <- tbl |>
+      modify_table_body(
+        function(tb) {
+          tb |>
+            mutate(idx = dplyr::row_number()) |>
+            dplyr::group_by(dplyr::pick(all_ard_groups(), "variable")) |>
+            dplyr::group_split() |>
+            map(function(dat) {
+              if (dat$variable[1] != var) {
+                dat
+              } else {
+                dat$idx_lvl <- NA
+                if (any(unlist(dat$label) %in% lvls)) {
+                  dat$idx_lvl[is.na(dat$idx_lvl) & !unlist(dat$label) %in% gp_nms] <-
+                    sapply(
+                      dat$label[is.na(dat$idx_lvl) & !unlist(dat$label) %in% gp_nms],
+                      \(x) which(lvls == unlist(x))
+                    )
+                }
+                if (any(unlist(dat$label) %in% gp_nms)) {
+                  dat$idx_lvl[unlist(dat$label) %in% gp_nms] <-
+                    sapply(
+                      dat$label[unlist(dat$label) %in% gp_nms],
+                      \(x) min(which(lvls %in% gps[[unlist(x)]])) - 0.5
+                    )
+                }
+                dat |>
+                  dplyr::arrange("idx_lvl", "idx") |>
+                  mutate(idx = seq(min(.data$idx), max(.data$idx))) |>
+                  select(-"idx_lvl")
+              }
+            }) |>
+            dplyr::bind_rows() |>
+            dplyr::arrange("idx") |>
+            select(-"idx")
         }
-      }) |>
-      bind_rows() |>
-      arrange(idx) |>
-      select(-idx) |>
-      as_card()
-
-    tbl <- tbl_ard_hierarchical(
-      ard,
-      variables = variables,
-      by = by,
-      include = {{ include }},
-      statistic = {{ statistic }},
-      label = labels
-    )
+      )
   }
+
+  tbl <- tbl |>
+    # Add "- Any Grade -" rows
+    modify_table_body(
+      function(tb) {
+        tb |>
+          mutate(idx = dplyr::row_number()) |>
+          dplyr::group_by(dplyr::pick(all_ard_groups())) |>
+          dplyr::group_split() |>
+          map(
+            function(x) {
+              if (any(x$variable == variables[length(variables) - 1])) {
+                dplyr::bind_rows(
+                  x[1, ] |>
+                    mutate(across(all_stat_cols(), ~ if (.data$variable %in% include) . else NA)),
+                  x[1, ] |>
+                    mutate(variable = var, label = "- Any Grade -", idx = .data$idx + 0.5),
+                  x[-1, ]
+                ) |>
+                  dplyr::filter(!.data$label %in% levels_exclude)
+              } else {
+                x
+              }
+            }
+          ) |>
+          dplyr::bind_rows() |>
+          dplyr::filter(!(.data$label == "- Any adverse events -" & .data$idx == 2)) |>
+          dplyr::arrange("idx") |>
+          select(-"idx")
+      }
+    ) |>
+    # Remove statistics from rows for variables not in include
+    modify_table_body(
+      \(x) {
+        x |>
+          dplyr::rowwise() |>
+          mutate(across(all_stat_cols(), ~ if (.data$variable %in% include) . else NA))
+      }
+    )
 
   tbl
 }
