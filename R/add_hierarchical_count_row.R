42241aefd8437be8148dfe95039221400de34ccc
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2025-06-05 16:30:52 -0700

    Adding `add_hierarchical_count_row()`

---
+++ R/add_hierarchical_count_row.R
@@ -0,0 +1,61 @@
+#' Add row with counts
+#'
+#' Typically used to add a row with overall AE counts to a table that primarily
+#' displays AE rates.
+#'
+#' @param x (`gtsummary`)\cr
+#'  a gtsummary table
+#' @param label (`string`)\cr
+#'  label for the new row
+#' @param .before,.after (`integer`)\cr
+#'  Row index where to add the new row. Default is after last row.
+#'
+#' @returns gtsummary table
+#' @export
+#'
+#' @examples
+#' # Example 1 ----------------------------------
+#' cards::ADAE |>
+#'   # subset the data for a shorter example table
+#'   dplyr::slice(1:10) |>
+#'   tbl_hierarchical(
+#'     by = "TRTA",
+#'     variables = AEDECOD,
+#'     denominator = cards::ADSL |> dplyr::rename(TRTA = TRT01A),
+#'     id = "USUBJID",
+#'     overall_row = TRUE
+#'   ) |>
+#'   add_hierarchical_count_row(.after = 1L)
+add_hierarchical_count_row <- function(x, label = "Overall total number of events", .before = NULL, .after = NULL) {
+  # check inputs ---------------------------------------------------------------
+  set_cli_abort_call()
+  check_class(x, "gtsummary")
+  check_string(label)
+  check_scalar_integerish(.before, allow_empty = TRUE)
+  check_scalar_integerish(.after, allow_empty = TRUE)
+
+  # create one row summary table -----------------------------------------------
+  tbl_count_one <-
+    x$inputs$data |>
+    dplyr::mutate(...row_count... = TRUE) |>
+    gtsummary::tbl_summary(
+      by = x$inputs$by,
+      include = "...row_count...",
+      type = list(...row_count... = "continuous"),
+      statistic = list(...row_count... = "{sum}"),
+      digits = list(...row_count... = gtsummary::label_style_number()),
+      label = list(...row_count... = label)
+    )
+
+  # add row to primary table ---------------------------------------------------
+  x$table_body <-
+    x$table_body |>
+    dplyr::add_row(
+      tbl_count_one$table_body[intersect(names(x$table_body), names(tbl_count_one$table_body))],
+      .before = .before,
+      .after = .after
+    )
+
+  # return table ---------------------------------------------------------------
+  x
+}
