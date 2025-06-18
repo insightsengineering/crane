a0182e5e6d76ff4a326e578fb1363bacf451b95a
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2025-06-18 11:30:38 -0700

    Adding `modify_zero_recode()`

--- R/tbl_demographics.R
+++ R/tbl_demographics.R
@@ -83,10 +83,7 @@ tbl_demographics <- function(data,
     # remove the default "Characteristic" header
     gtsummary::modify_header(label = "") |>
     # convert "0 (0.0%)" to "0"
-    gtsummary::modify_post_fmt_fun(
-      fmt_fun = ~ ifelse(. %in% c("0 (0.0%)", "0 (NA%)"), "0", .),
-      columns = all_stat_cols()
-    ) |>
+    modify_zero_recode() |>
     # sort the missing row to just below the header row
     gtsummary::modify_table_body(
       function(.x) {
