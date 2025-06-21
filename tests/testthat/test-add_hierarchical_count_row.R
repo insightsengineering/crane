833a1b0750e9292bb34a391e1b7cd8b3d1b74e25
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-06-20 17:20:34 -0700

    accounting for `add_overall()` in `add_hierarchical_count_row()` (#70)
    **What changes are proposed in this pull request?**
    * Style this entry in a way that can be copied directly into `NEWS.md`.
    (#<issue number>, @<username>)
    
    Provide more detail here as needed.
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #<issue number>'_
    closes #69
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [ ] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".

--- tests/testthat/test-add_hierarchical_count_row.R
+++ tests/testthat/test-add_hierarchical_count_row.R
@@ -1,15 +1,27 @@
 test_that("add_hierarchical_count_row() works", {
-  expect_snapshot(
+  tbl <-
     cards::ADAE |>
-      # subset the data for a shorter example table
-      dplyr::slice(1:10) |>
-      tbl_hierarchical(
-        by = "TRTA",
-        variables = AEDECOD,
-        denominator = cards::ADSL |> dplyr::rename(TRTA = TRT01A),
-        id = "USUBJID",
-        overall_row = TRUE
-      ) |>
+    # subset the data for a shorter example table
+    dplyr::slice(1:10) |>
+    tbl_hierarchical(
+      by = "TRTA",
+      variables = AEDECOD,
+      denominator = cards::ADSL |> dplyr::rename(TRTA = TRT01A),
+      id = "USUBJID",
+      overall_row = TRUE
+    )
+
+  expect_snapshot(
+    tbl |>
+      add_hierarchical_count_row(.after = 1L) |>
+      as.data.frame(col_label = FALSE) |>
+      dplyr::slice(2)
+  )
+
+  # check results include overall column
+  expect_snapshot(
+    tbl |>
+      add_overall() |>
       add_hierarchical_count_row(.after = 1L) |>
       as.data.frame(col_label = FALSE) |>
       dplyr::slice(2)
