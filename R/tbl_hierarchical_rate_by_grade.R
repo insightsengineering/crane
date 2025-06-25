a62a8c320dfa903f83751d4afe5a1f2de4b75224
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-06-25 16:01:57 -0400

    fmt_fn -> fmt_fun

--- R/tbl_hierarchical_rate_by_grade.R
+++ R/tbl_hierarchical_rate_by_grade.R
@@ -254,14 +254,14 @@ tbl_hierarchical_rate_by_grade <- function(data,
     dplyr::rows_update(
       imap(
         digits,
-        ~ enframe(.x, "stat_name", "fmt_fn") |>
+        ~ enframe(.x, "stat_name", "fmt_fun") |>
           dplyr::mutate(variable = .y)
       ) |>
         dplyr::bind_rows(),
       by = c("variable", "stat_name"),
       unmatched = "ignore"
     ) |>
-    cards::apply_fmt_fn()
+    cards::apply_fmt_fun()
 
   # generate table from the final ARD ------------------------------------------
   tbl_final <-
