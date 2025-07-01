0c108a52080deba039f8fa7c766a65e1b869e025
melkiades <davide.garolini@roche.com>
2025-07-01 18:26:14 +0200

    forgot

--- R/add_blank_row.R
+++ R/add_blank_row.R
@@ -97,7 +97,6 @@ add_blank_row <- function(x, variables = NULL, row_numbers = NULL) {
   # add blank row --------------------------------------------------------------
   # get row indices where to add blank row
   if (!is_empty(variables)) {
-    browser()
     row_indices <-
       x$table_body["variable"] |>
       dplyr::mutate(row_number = dplyr::row_number()) |>
