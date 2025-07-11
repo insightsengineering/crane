87a7f9789f10f79ec1a59e84e9e35977b4057ddd
ayogasekaram <abinaya.yogasekaram@contractors.roche.com>
2025-07-11 13:54:20 +0000

    update parameter description

--- R/tbl_hierarchical_rate_and_count.R
+++ R/tbl_hierarchical_rate_and_count.R
@@ -22,6 +22,7 @@
 #'   Variables must be specified in the nesting order.
 #' @param digits ([`formula-list-selector`][gtsummary::syntax])\cr
 #'  Specifies how summary statistics are rounded. Values may be either integer(s) or function(s).
+#'  If a theme is applied, the `digits` specifications of the theme is applied.
 #' @param label_overall_rate (`string`)\cr
 #'  String for the overall rate summary. Default is
 #'  `"Total number of participants with at least one adverse event"`.
