d8a10e8d0dae2d80cddb310d6b8ae278274b466a
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-08-14 18:25:21 -0700

    `modify_zero_recode()` to recode `0 / nn (0%)` to `0 / nn` (#103)
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [ ] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".

--- tests/testthat/test-modify_zero_recode.R
+++ tests/testthat/test-modify_zero_recode.R
@@ -15,4 +15,26 @@ test_that("modify_zero_recode() works", {
       modify_zero_recode() |>
       as.data.frame()
   )
+
+  expect_true(
+    dplyr::tribble(
+      ~stat_0, ~recode_value,
+      "0 (0%)", "0",
+      "0 (0.0%)", "0",
+      "0 (NA%)", "0",
+      "0 / 12 (0%)", "0 / 12",
+      "0 / 2 (0.0%)", "0 / 2",
+      "0 / 0 (NA%)", "0 / 0",
+      "0/12 (0%)", "0/12",
+      # items NOT to be re-coded (since there is no space in front of the percentage)
+      "0 / 12(0%)", "0 / 12(0%)",
+      "0(0%)", "0(0%)"
+    ) |>
+      gtsummary::as_gtsummary() |>
+      gtsummary::modify_column_unhide(everything()) |>
+      modify_zero_recode() |>
+      as.data.frame() %>%
+      {.[, 1] == .[, 2]} |> # styler: off
+      all()
+  )
 })
