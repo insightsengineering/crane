edd907fdad0e8f21dfa8bbcfc7c0f95f33954b9b
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2025-08-29 12:56:13 -0700

    Added function `style_roche_ratio()`

--- R/label_roche.R
+++ R/label_roche.R
@@ -3,30 +3,38 @@
 #' @description
 #' - `label_roche_pvalue()` returns formatted p-values.
 #' - `label_roche_percent()` returns formatted percent values. This function only formats percentages between 0 and 1.
+#' - `label_roche_ratio()` returns formatted ratios with values below and above a threshold being returned as `< 0.1` and `> 999.9`, for example, when `digits=1`.
 #'
 #' @return A character vector of rounded p-values
+#' @inheritParams gtsummary::style_number
 #' @inheritParams gtsummary::style_pvalue
 #' @examples
 #' # p-value formatting
 #' x <- c(0.0000001, 0.123456)
 #'
-#' roche_pvalue(x)
+#' style_roche_pvalue(x)
 #' label_roche_pvalue()(x)
 #'
 #' # percent formatting
 #' x <- c(0.0008, 0.9998)
 #'
-#' roche_percent(x)
+#' style_roche_percent(x)
 #' label_roche_percent()(x)
+#'
+#' # ratio formatting
+#' x <- c(0.0008, 0.8234, 2.123, 1000)
+#'
+#' style_roche_ratio(x)
+#' label_roche_ratio()(x)
 #' @name label_roche
 NULL
 
 #' @export
 #' @rdname label_roche
-roche_pvalue <- function(x,
-                         big.mark = ifelse(decimal.mark == ",", " ", ","),
-                         decimal.mark = getOption("OutDec"),
-                         ...) {
+style_roche_pvalue <- function(x,
+                               big.mark = ifelse(decimal.mark == ",", " ", ","),
+                               decimal.mark = getOption("OutDec"),
+                               ...) {
   dplyr::case_when(
     # allowing some leeway for numeric storage errors
     x > 1 + 1e-15 ~ NA_character_,
@@ -42,20 +50,19 @@ roche_pvalue <- function(x,
 label_roche_pvalue <- function(big.mark = ifelse(decimal.mark == ",", " ", ","),
                                decimal.mark = getOption("OutDec"),
                                ...) {
-  function(x) roche_pvalue(x, big.mark = big.mark, decimal.mark = decimal.mark, ...)
+  function(x) style_roche_pvalue(x, big.mark = big.mark, decimal.mark = decimal.mark, ...)
 }
 
-#' @inheritParams gtsummary::style_number
 #' @export
 #' @rdname label_roche
-roche_percent <- function(x,
-                          prefix = "",
-                          suffix = "",
-                          digits = 1,
-                          scale = 100,
-                          big.mark = ifelse(decimal.mark == ",", " ", ","),
-                          decimal.mark = getOption("OutDec"),
-                          ...) {
+style_roche_percent <- function(x,
+                                digits = 1,
+                                prefix = "",
+                                suffix = "",
+                                scale = 100,
+                                big.mark = ifelse(decimal.mark == ",", " ", ","),
+                                decimal.mark = getOption("OutDec"),
+                                ...) {
   dplyr::case_when(
     is.na(x) ~ NA_character_,
     abs(x - 1) < sqrt(.Machine$double.eps) ~ gtsummary::style_number(1, scale = 100, digits = digits - 1, big.mark = big.mark, decimal.mark = decimal.mark, suffix = suffix, ...),
@@ -78,11 +85,70 @@ roche_percent <- function(x,
 
 #' @export
 #' @rdname label_roche
-label_roche_percent <- function(big.mark = ifelse(decimal.mark == ",", " ", ","),
+label_roche_percent <- function(digits = 1,
                                 suffix = "",
                                 scale = 100,
-                                digits = 1,
+                                big.mark = ifelse(decimal.mark == ",", " ", ","),
                                 decimal.mark = getOption("OutDec"),
                                 ...) {
-  function(x) roche_percent(x, suffix = suffix, big.mark = big.mark, decimal.mark = decimal.mark, digits = digits, ...)
+  function(x) style_roche_percent(x, suffix = suffix, big.mark = big.mark, decimal.mark = decimal.mark, digits = digits, ...)
+}
+
+#' @export
+#' @rdname label_roche
+style_roche_ratio <- function(x,
+                              digits = 2,
+                              prefix = "",
+                              suffix = "",
+                              scale = 1,
+                              big.mark = ifelse(decimal.mark == ",", " ", ","),
+                              decimal.mark = getOption("OutDec"),
+                              ...) {
+  dplyr::case_when(
+    x < 10 ^ (-digits) ~
+      gtsummary::style_number(
+        x = 10 ^ (-digits),
+        digits = digits,
+        prefix = paste0(prefix, "< "),
+        suffix = suffix,
+        scale = scale,
+        big.mark = big.mark,
+        decimal.mark = decimal.mark,
+        ...
+      ),
+    x > 1000 - 10 ^ (-digits) ~
+      gtsummary::style_number(
+        x = 1000 - 10 ^ (-digits),
+        digits = digits,
+        prefix = paste0(prefix, "> "),
+        suffix = suffix,
+        scale = scale,
+        big.mark = big.mark,
+        decimal.mark = decimal.mark,
+        ...
+      ),
+    .default =
+      gtsummary::style_number(
+        x = x,
+        digits = digits,
+        prefix = prefix,
+        suffix = suffix,
+        scale = scale,
+        big.mark = big.mark,
+        decimal.mark = decimal.mark,
+        ...
+      )
+  )
+}
+
+#' @export
+#' @rdname label_roche
+label_roche_ratio <- function(digits = 2,
+                              prefix = "",
+                              suffix = "",
+                              scale = 1,
+                              big.mark = ifelse(decimal.mark == ",", " ", ","),
+                              decimal.mark = getOption("OutDec"),
+                              ...) {
+  function(x) style_roche_ratio(x, prefix = prefix, suffix = suffix, big.mark = big.mark, decimal.mark = decimal.mark, digits = digits, ...)
 }
--- tests/testthat/test-label_roche.R
+++ tests/testthat/test-label_roche.R
@@ -1,27 +1,41 @@
-test_that("roche_pvalue() works", {
+test_that("style_roche_pvalue() works", {
   expect_equal(
     c(-1, 0, 0.0000000001, 0.5, 0.9, 1, 1.1) |>
-      roche_pvalue(),
+      style_roche_pvalue(),
     c(NA, "<0.0001", "<0.0001", "0.5000", "0.9000", "1.0000", NA)
   )
 })
 
-test_that("roche_percent() works", {
+test_that("style_roche_percent() works", {
   expect_equal(
     c(1, 0.0008, 0.9998, 0.998, 0.5, -1, 1.1, 0) |>
-      roche_percent(suffix = "%"),
+      style_roche_percent(suffix = "%"),
     c("100%", "<0.1%", ">99.9%", "99.8%", "50.0%", NA, NA, "0.0%")
   )
 
   expect_equal(
     c(1, 0.0008, 0.9998, 0.998, 0.5, -1, 1.1, 0) |>
-      roche_percent(suffix = "%", digits = 2),
+      style_roche_percent(suffix = "%", digits = 2),
     c("100.0%", "<0.10%", ">99.90%", "99.80%", "50.00%", NA, NA, "0.00%")
   )
 
   expect_equal(
     c(1, 0.0008, 0.9998, 0.998, 0.5, -1, 1.1, 0) |>
-      roche_percent(suffix = "%", digits = 4),
+      style_roche_percent(suffix = "%", digits = 4),
     c("100.000%", "<0.1000%", ">99.9000%", "99.8000%", "50.0000%", NA, NA, "0.0000%")
+  )
+})
+
+test_that("style_roche_ratio() works", {
+  x <- c(0.0008, 0.8234, 2.123, 1000)
+
+  expect_equal(
+    style_roche_ratio(x),
+    c("< 0.01", "0.82", "2.12", "> 999.99")
+  )
+
+  expect_equal(
+    label_roche_ratio()(x),
+    c("< 0.01", "0.82", "2.12", "> 999.99")
   )
 })
