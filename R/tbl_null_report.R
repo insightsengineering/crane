7d71f63c6e36c966096ba80018aa4f7d2fac6cc1
melkiades <davide.garolini@roche.com>
2025-09-30 19:02:38 +0200

    fixes

--- R/tbl_null_report.R
+++ R/tbl_null_report.R
@@ -2,28 +2,30 @@
 #'
 #' This function creates a null report for tables or listings without any statistics.
 #'
+#' @param label (`string`)\cr label to display in the header of the null report. It defaults to
+#'   "Null Report: no observations met the reporting criteria for inclusion in this output."
+#'
 #' @examples
-#' tbl_null_report()
+#' tbl_null_report(label = "No data available for the selected criteria.")
 #'
 #' @export
 #' @rdname tbl_null_report
-tbl_null_report <- function() {
+tbl_null_report <- function(
+    label = "Null Report: no observations met the reporting criteria for inclusion in this output.") {
   set_cli_abort_call()
 
-  # Create empty gtsummary object
-  x <- gtsummary::as_gtsummary(data.frame(blank = character())) |>
-    gtsummary::modify_header(
-      blank = "Null Report: no observations met the reporting criteria for inclusion in this output."
-    )
+  # Check input label ----------------------------------------------------------
+  check_string(label)
+
+  # Create empty gtsummary object ----------------------------------------------
+  x <- gtsummary::as_gtsummary(data.frame(label = character())) |>
+    gtsummary::modify_header(label = label)
 
   # add class and attributes ---------------------------------------------------
   x <- structure(
     x,
     class = c("tbl_null_report", "gtsummary")
   )
-
-  # add inputs as attribute
-  attr(x, "inputs") <- as.list(environment())
 
   x
 }
