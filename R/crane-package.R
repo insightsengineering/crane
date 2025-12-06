0d8a0e4801493a89e07a273d6d86eb2758c33230
Joe Zhu <sha.joe.zhu@gmail.com>
2025-12-06 18:06:15 +0800

    update import

--- R/crane-package.R
+++ R/crane-package.R
@@ -3,6 +3,7 @@
 #' @import glue glue
 #' @import ggplot2
 #' @import patchwork
+#' @importFrom broom.helpers tidy_plus_plus
 #' @importFrom dplyr across starts_with ends_with contains matches num_range
 #'                   all_of any_of everything last_col where
 "_PACKAGE"
