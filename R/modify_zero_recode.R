5bd3fc1cf0241bcc7a38ee5aa79cbbdb9a42a4b1
Jan Szczypiński <jan.szczypinski@external.roche.com>
2026-05-05 12:06:39 +0200

    Return the part discarded by gem

--- R/modify_zero_recode.R
+++ R/modify_zero_recode.R
@@ -16,6 +16,23 @@
 #' @details
 #' The function is a wrapper for `gtsummary::modify_post_fmt_fun()`.
 #'
+#'
+#' ```r
+#' gtsummary::modify_post_fmt_fun(
+#'   x,
+#'   fmt_fun = \(x) {
+#'     dplyr::case_when(
+#'       # convert "0 (0%)" OR "0 (0.0%)" OR 0 (NA%) to "0"
+#'       str_detect(x, "^0\\s\\((?:0(?:\\.0)?|NA)%\\)$") ~ str_remove(x, pattern = "\\s\\((?:0(?:\\.0)?|NA)%\\)$"),
+#'       # convert "0 / nn (0%)" OR "0/nn (0.0%)" OR 0/0 (NA%) to "0 / nn" OR "0/nn" OR "0/0"
+#'       str_detect(x, pattern = "^(0 ?/) ?\\d+[^()]* \\((?:0(?:\\.0)?|NA)%\\)$") ~ str_remove(x, pattern = "\\s\\((?:0(?:\\.0)?|NA)%\\)$"),
+#'       .default = x
+#'     )
+#'   },
+#'   columns = gtsummary::all_stat_cols()
+#' )
+#' ```
+#'
 #' @param x (`gtsummary`)\cr
 #'   a gtsummary table
 #'
