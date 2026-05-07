61294e2614a53c8231a52694befa60d8fc8e91fa
Jan Szczypiński <jan.szczypinski@external.roche.com>
2026-05-07 14:29:19 +0200

    More roxy

--- R/crane-package.R
+++ R/crane-package.R
@@ -4,8 +4,8 @@
 #' @import glue glue
 #' @importFrom broom tidy
 #' @importFrom cowplot plot_grid ggdraw draw_plot
-#' @importFrom dplyr across starts_with ends_with contains matches num_range
-#'                   all_of any_of everything last_col where mutate
+#' @importFrom dplyr across starts_with ends_with contains matches num_range 
+#' @importFrom dplyr all_of any_of everything last_col where mutate
 #' @importFrom labeling extended
 #' @importFrom survival coxph Surv survdiff
 #' @importFrom stats pchisq median
